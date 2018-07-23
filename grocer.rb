b87a5fd3ec648da9a4fd6e349d5ab99d0d3df5e2
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-23 19:51:53 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -69,13 +69,9 @@ end
 def apply_clearance(cart)
  
  cart.each do |item_name, attributes|
-   puts cart
-   puts item_name.class
    if attributes[:clearance] == true
      attributes[:price] = (0.8*attributes[:price]).round(2)
- 
- 
- end
+   end
  end
  cart
 end
